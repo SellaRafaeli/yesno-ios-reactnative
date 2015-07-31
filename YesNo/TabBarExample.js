44d6931496dd7ba76f3e17c227b671040bc7cb3b
Sella Rafaeli <sella.rafaeli@gmail.com>
2015-08-01 01:00:41 +0300

    row of posts, minor styling, onpress row

--- YesNo/TabBarExample.js
+++ YesNo/TabBarExample.js
@@ -10,10 +10,9 @@ var {
 } = React;
 
 function getPosts(obj) {  
-  console.log("getPosts")
-  fetch('http://localhost:9292/ping').then(response => obj.handleNewPosts(response));
-  // .then(response => response.json())
-  // .then(json => obj.handleNewPosts(json.response));
+  console.log("getPosts");
+  var postsRoute = 'http://localhost:9292/mobile/posts/?opts[page]=1&opts[type]=user_posts_type&opts[feed]=true&mobile_token=9907e080-5d1b-400c-82c1-8d1bb9409b29'
+  fetch(postsRoute).then(response => response.json()).then(json => obj.handleNewPosts(json));
   }
 
 
@@ -28,7 +27,7 @@ var TabBarExample = React.createClass({
   getInitialState: function() {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     var arr = [];
-    for (var i=0; i<100; i++) { console.log(i); arr.push("row "+i) }
+    for (var i=0; i<30; i++) { console.log(i); arr.push("row and now his watch has ended. For Olly! Hooray hooray hooray and some more words. Thank you for the music! -- "+i) }
     return {
       selectedTab: 'redTab',
       dataSource: ds.cloneWithRows(arr),
@@ -39,17 +38,37 @@ var TabBarExample = React.createClass({
   },
 
   handleNewPosts: function(postsJson) {
-    console.log("handleNewPosts");    
-    this.setState({posts: postsJson})
+    console.log("handleNewPosts");     
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.setState({dataSource: ds.cloneWithRows(postsJson.posts)});
   },
 
+  _renderRow: function(rowData) {
+    return (
+      <View
+        onPress={() => {
+          debugger
+            console.log("pressed row view!");
+          }}
+      >
+      <Text style={styles.item_q}
+        onPress={() => {
+          debugger
+            console.log("pressed row text!");
+          }}
+      >{rowData.q_text}</Text>
+      <Text style={styles.item_a}>{rowData.a_text}</Text>
+      </View>
+    )
+  },
   _renderContent: function(color: string, pageText: string, num?: number) {
 
     return (
       <View style={[styles.tabContent, {backgroundColor: color}]}>
        <ListView
+      style={styles.listView}
       dataSource={this.state.dataSource}
-      renderRow={(rowData) => <Text>{rowData}</Text>}
+      renderRow={(rowData) => this._renderRow(rowData)}
     />
       { /*  <Text style={styles.tabText}>{pageText}</Text>
         <Text style={styles.tabText}>{this.state.posts}</Text>
@@ -98,7 +117,7 @@ var TabBarExample = React.createClass({
               presses: this.state.presses + 1
             });
           }}>
-          {this._renderContent('#21551C', 'Green Tab', this.state.presses)}
+          {this._renderContent('beige', 'Green Tab', this.state.presses)}
         </TabBarIOS.Item>
 
       </TabBarIOS>
@@ -111,6 +130,25 @@ var styles = StyleSheet.create({
   tabContent: {
     flex: 1,
     alignItems: 'center',
+  },
+  listView: {
+    // flex: 0.5,
+    // padding: 40,
+    marginTop: 45,
+    // width: 400,
+
+    //alignItems: 'right'
+  },
+  item_q: {
+    color: 'blue'
+  },
+  item_a: {
+    
+    //backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 0,
+    backgroundColor: '#F6F6F6',
   },
   tabText: {
     color: 'white',
